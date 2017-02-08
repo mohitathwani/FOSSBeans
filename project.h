08b80d0b4933f26296912a960ca2210529d0b638
Pinak Das <pinakdas163@gmail.com>
2017-02-08 11:41:45 -0800

    Added Project Interface

---
+++ project.h
@@ -0,0 +1,24 @@
+/* 
+  @file: project.h
+  @author:
+	Pinak Das
+	Tanay Khandke
+*/
+#ifndef PROJECT_H
+#define PROJECT_H
+
+class Project {
+  private:
+    int _submitted;
+	int _accepted;
+    string _name;
+  public:
+    Project(string projectName);
+    void submitRequest();
+	void acceptRequest();
+    int getSubmittedRequests();
+	int getAcceptedRequests();
+	string getName();
+}
+
+#endif
